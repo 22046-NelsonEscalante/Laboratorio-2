216f41439f81fec36296258517bfd842b5a43d1f
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2022-09-24 15:23:30 -0600

    Added the properties to Program.java

---
+++ model/Program.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Program {
+    private String name;
+    private int space;
+    private int useTime;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSpace() {
+        return space;
+    }
+    public void setSpace(int space) {
+        this.space = space;
+    }
+    public int getUseTime() {
+        return useTime;
+    }
+    public void setUseTime(int useTime) {
+        this.useTime = useTime;
+    }
+}
